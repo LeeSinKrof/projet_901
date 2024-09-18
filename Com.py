1f3118a5eb597d0f9c5c6f4665f599b6c917fe26
LeeSinKrof <ceylan.emre@laposte.net>
2024-09-18 15:49:26 +0200

    add message class

--- Com.py
+++ Com.py
@@ -1,55 +1,13 @@
-import threading
+from threading import Lock, Semaphore
+from time import sleep
 
-from pyeventbus3.pyeventbus3 import *
-from Message import *
+from Message import Message
 
 class Com:
-    def __init__(self, clock):
-        self.clock = 0
-        self.message = None
-        self.nbProcess = None
-        self.myId = None
-        self.mailbox = None
+    def __init__(self):
+        self.lamport_clock = 0
+        self.clock_lock = Semaphore()
 
     def inc_clock(self):
-        self.clock += 1
-
-    def getMessage(self):
-        return self.message
-
-
-    def getClock(self):
-        return self.clock
-
-    def getNbProcess(self):
-        return self.nbProcess
-
-    def getMyId(self):
-        return self.myId
-
-    def getMailbox(self):
-        return self.mailbox
-
-    def sendTo(self, message, to):
-        pass
-
-    def sendToSync(self, message, to):
-        pass
-
-    def recevFromSync(self, message, fromId):
-        pass
-
-    def synchronize(self):
-        pass
-
-    def requestSC(self):
-        pass
-
-    def releaseSC(self):
-        pass
-
-    def broadcast(self, message):
-        pass
-
-
-
+        with self.clock_lock:
+            self.lamport_clock += 1
--- Message.py
+++ Message.py
@@ -1,8 +1,22 @@
-class Message:
-    def __init__(self, message, sender, to):
+from abc import ABC
+
+class Message(ABC):
+    def __init__(self, sender, message, receiver, stamp):
+        self.sender_id = sender
         self.message = message
-        self.sender = sender
-        self.to = to
+        self.receiver = receiver
+        self.stamp = stamp
 
+class sendTo(Message):
+    def __init__(self, sender, message, receiver, stamp):
+        super().__init__(sender, message, receiver, stamp)
 
+    def send_message(self):
+        print(f"Sending message '{self.message}' to {self.receiver}")
 
+class broadcast(Message):
+    def __init__(self, sender, message, stamp):
+        super().__init__(sender, message, stamp)
+
+    def send_message(self):
+        print(f"Broadcasting message '{self.message}' to all")
