6ef09bcb7440ec523bd910c3261e7292f0b1372e
LeeSinKrof <ceylan.emre@laposte.net>
2024-09-18 16:19:37 +0200

    add broadcast and sendTo function

--- Com.py
+++ Com.py
@@ -1,13 +1,59 @@
 from threading import Lock, Semaphore
 from time import sleep
 
-from Message import Message
+from Message import *
+
+from pyeventbus3.pyeventbus3 import *
 
 class Com:
-    def __init__(self):
-        self.lamport_clock = 0
-        self.clock_lock = Semaphore()
+    nbProcessCreated = 0
+    def __init__(self, nbProcess, clock):
+        self.clock = clock
+        self.nbProcess = nbProcess
+        self.myId = Com.nbProcessCreated
+        Com.nbProcessCreated += 1
+        self.mailbox = []
+        self.sem = Semaphore()
 
     def inc_clock(self):
-        with self.clock_lock:
-            self.lamport_clock += 1
+        with self.clock:
+            self.sem.acquire()
+            self.clock += 1
+            self.sem.release()
+
+    def getNbProcess(self):
+        return self.nbProcess
+
+    def getMyId(self):
+        return self.myId
+
+    @subscribe(threadMode=Mode.PARALLEL, onEvent=sendMessageTo)
+    def onReceive(self, event):
+        if event.receiver == self.getMyId():
+            if event.stamp > self.clock:
+                self.clock = event.stamp
+            else:
+                self.inc_clock()
+            self.mailbox.append(event)
+            print(f"{event.receiver} received message from {event.sender}: {event.message}")
+
+    def sendTo(self, msg, to):
+        self.inc_clock()
+        message = sendMessageTo(self.myId, msg, to, self.clock)
+        PyBus.Instance().post(message)
+
+    @subscribe(threadMode=Mode.PARALLEL, onEvent=broadcastMessage)
+    def onBroadcast(self, event):
+        if event.sender != self.myId:
+            if event.clock > self.clock:
+                self.clock = event.clock
+            else:
+                self.inc_clock()
+            if event not in self.mailbox:
+                self.mailbox.append(event)
+            print(f"{event.sender} received broadcasted message: {event.message}")
+
+    def broadcast(self, msg):
+        self.inc_clock()
+        message = broadcastMessage(self.myId, msg, self.clock)
+        PyBus.Instance().post(message)
--- Message.py
+++ Message.py
@@ -7,16 +7,10 @@ class Message(ABC):
         self.receiver = receiver
         self.stamp = stamp
 
-class sendTo(Message):
+class sendMessageTo(Message):
     def __init__(self, sender, message, receiver, stamp):
         super().__init__(sender, message, receiver, stamp)
 
-    def send_message(self):
-        print(f"Sending message '{self.message}' to {self.receiver}")
-
-class broadcast(Message):
+class broadcastMessage(Message):
     def __init__(self, sender, message, stamp):
         super().__init__(sender, message, stamp)
-
-    def send_message(self):
-        print(f"Broadcasting message '{self.message}' to all")
